36852b6191101b5ce1fcaf8920a0bf6c8deb246b
diegiyo138@gmail.com <diegiyo138@gmail.com>
2026-05-07 22:59:47 -0600

    Multiplicar AB 07/05/2026

---
+++ IntroduccionCsharp/Controllers/MultiplicarController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IntroduccionCsharp.Models;
+
+namespace IntroduccionCsharp.Controllers
+{
+    public class MultiplicarController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(new MultiplicacionSuma());
+        }
+
+        [HttpPost]
+        public ActionResult Index(MultiplicacionSuma ms)
+        {
+            ms.Multiplicar();
+            return View(ms);
+        }
+    }
+}
---
+++ IntroduccionCsharp/Models/MultiplicacionSuma.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace IntroduccionCsharp.Models
+{
+    public class MultiplicacionSuma
+    {
+        public int Num1 { get; set; }
+        public int Num2 { get; set; }
+        public int Res { get; set; }
+
+        public void Multiplicar()
+        {
+            Res = 0;
+            // for para hacer la multiplicacion con sumas
+            for (int i = 0; i < Num2; i++)
+            {
+                Res += Num1;
+            }
+        }
+    }
+}
